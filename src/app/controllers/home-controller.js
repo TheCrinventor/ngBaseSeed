103dc1c96ab7da357d0ffdee2dca10b5a40aacea
Anvay <thecrinventor@gmail.com>
2016-08-11 12:45:13 +0530

    additional feature

--- src/app/controllers/home-controller.js
+++ src/app/controllers/home-controller.js
@@ -3,13 +3,17 @@ angular.module('Base')
         var vm = $scope;
         vm.getData = getData;
         vm.WelcomeMsg = 'Hello and welcome to Savvy';
-        
+        vm.unauthorizedAccess = false;
         vm.stats = { transaction_status: { '2': 4, '3': 6}};
         
         function getData(){
             dataService.get('http://api.investsavvy.in:81/v1/workflows').then(function(data){
                 vm.stats = data;
                 console.log(data);
+                vm.unauthorizedAccess = false;
+            }, function(status){
+                console.log("This is rejected:" + status);
+                vm.unauthorizedAccess = true;
             });
         }
         
